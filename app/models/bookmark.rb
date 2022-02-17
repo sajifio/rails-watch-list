e1e682331804c296e73b9c35dd93a355d12c2a01
Fiorella Salazar <sajifio@gmail.com>
2022-02-17 13:11:25 +0000

    add validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,4 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list, dependent: :destroy
   validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id }
 end
